63134f34bf9d7d7f145ba74bb7cb3cf246da28d2
bartekzielinski <bartek.ziel.agh@gmail.com>
2023-06-19 14:21:49 +0200

    upiększenie pliku snake.cpp

--- snake/src/snake.cpp
+++ snake/src/snake.cpp
@@ -1,35 +1,32 @@
 #include "snake.hpp"
 
-void Plansza::rysuj(){
+void Plansza::rysuj() {
     //rysowanie góry ramki
-    std::cout<< char(201);
-    for ( int i = 0; i < x_ ; i++)
-        std::cout<< char(205) <<char(205) ;
-    std::cout<<char(187)<<std::endl;
+    std::cout << char(201);
+    for (int i = 0; i < x_; i++)
+        std::cout << char(205) << char(205);
+    std::cout << char(187) << std::endl;
     //rysowanie boków ramki
-    for (int i = 0; i < y_ ; i++ )
-    {
+    for (int i = 0; i < y_; i++) {
         std::cout << char(186);
         for (int j = 0; j < x_; j++)
             std::cout << "  ";
-        std::cout << char(186) <<std::endl;
+        std::cout << char(186) << std::endl;
     }
     //rysowanie dołu ramki
-    std::cout<< char(200);
-    for ( int i = 0; i < x_ ; i++)
-        std::cout<< char(205) <<char(205) ;
-    std::cout<< char(188);
+    std::cout << char(200);
+    for (int i = 0; i < x_; i++)
+        std::cout << char(205) << char(205);
+    std::cout << char(188);
 }
 
-void Snake::usun_ogon()
-{
+void Snake::usun_ogon() {
     idzdoxy(x_k * 2 + 1, y_k + 1);
-    std::cout<<"  ";
+    std::cout << "  ";
 }
 
-void Snake::rysuj_glowe()
-{
+void Snake::rysuj_glowe() {
     idzdoxy(x_ * 2 + 1, y_ + 1);
-    std::cout<<this->pole_;
+    std::cout << this->pole_;
 }
 
